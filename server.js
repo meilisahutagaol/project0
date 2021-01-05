5b50f01d6f6929e2c66e3e8e946e2d2a3b43b795
capstone_2020 <meilisahutagaol@mail.ugm.ac.id>
2021-01-05 22:01:59 +0700

    commit

---
+++ server.js
@@ -0,0 +1,10 @@
+﻿const express = require ("express");
+const path = require ("path");
+
+const app = express();
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve("frontend", "upload.html"));
+});
+
+app.listen (process.env.PORT || 5060, () => console.log ("Server running..."));
